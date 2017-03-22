b26daba2658e3d669eb9ec15a3d6599a55c94ed8
emperor31 <sairamsingireesu@gmail.com>
2017-03-23 01:41:19 +0530

    explicit array for xml parser

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var fs= require('fs');
 var xml2js= require('xml2js');
 
-var xmlParser= new xml2js.Parser();
+var xmlParser= new xml2js.Parser({explicitArray:false});
 
 fs.readFile('fra.xml',function(err,str){
 	console.log(err);
-	console.log(str);
+	// console.log(str);
 	xmlParser.parseString(str,function(err,xmlObj){
-		console.log(xmlObj);
+		console.log(xmlObj.messages.note[1].heading);
 	})
 });
