842120d8ca396181b47a79af7dba49706f403e03
unknown <johnconnor31@github.com>
2017-03-23 21:04:02 +0530

    deal entry xml read

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ var fs= require('fs');
 var xml2js= require('xml2js');
 
 var xmlParser= new xml2js.Parser({explicitArray:false});
+var iParser= new xml2js.Parser({explicitArray:false});
 
-fs.readFile('fra.xml',function(err,str){
+fs.readFile('fra1.xml',function(err,str){
 	console.log(err);
 	// console.log(str);
 	xmlParser.parseString(str,function(err,xmlObj){
-		console.log(xmlObj.messages.note[1].heading);
+		console.log(xmlObj.grid.rows.row[1].$.xmlTag);
+	})
+});
+
+
+fs.readFile('inputFields.xml',function(err,str){
+	console.log(err);
+	console.log(str);
+	iParser.parseString(str,function(err,xmlObj){
+		console.log(xmlObj.OpicsPlusRequest.Header.Message.Screens.Screen[3].DE);
 	})
 });
