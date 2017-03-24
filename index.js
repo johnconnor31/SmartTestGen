5b254457b2cfdd1ce7b6a9f9611fb10a8f07383a
unknown <johnconnor31@github.com>
2017-03-24 17:29:30 +0530

    looping

--- index.js
+++ index.js
@@ -3,20 +3,34 @@ var xml2js= require('xml2js');
 
 var xmlParser= new xml2js.Parser({explicitArray:false});
 var iParser= new xml2js.Parser({explicitArray:false});
-
+var templateFields;
 fs.readFile('fra1.xml',function(err,str){
 	console.log(err);
 	// console.log(str);
 	xmlParser.parseString(str,function(err,xmlObj){
-		console.log(xmlObj.grid.rows.row[1].$.xmlTag);
+		// console.log(xmlObj.grid.rows.row);
+		templateFields= xmlObj.grid.rows.row;
 	})
 });
 
 
 fs.readFile('inputFields.xml',function(err,str){
 	console.log(err);
-	console.log(str);
+	// console.log(str);
 	iParser.parseString(str,function(err,xmlObj){
-		console.log(xmlObj.OpicsPlusRequest.Header.Message.Screens.Screen[3].DE);
+		// console.log(xmlObj.OpicsPlusRequest.Header.Message.Screens.Screen[3].DE.H_Value1);
+		generateCases(xmlObj.OpicsPlusRequest.Header.Message.Screens.Screen[3].DE.H_Value1);
 	})
 });
+
+function generateCases(inputParams){
+	templateFields.map(function(field){
+		console.log(field);
+		
+	})
+
+}
+
+function writeToExcel(vals){
+	console.log(vals);
+}
